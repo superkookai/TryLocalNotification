1e44575829698fe197423ffdaedd1247b55daac2
Weerawut <weerawut@live.com>
2024-12-22 12:44:11 +0700

    UserNotifications

--- TryLocalNotification/ContentView.swift
+++ TryLocalNotification/ContentView.swift
@@ -6,16 +6,50 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
+    @State private var authorised: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "bell.badge.fill")
+                .font(.system(size: 150))
+                .foregroundStyle(.purple,.black)
+            
+            if authorised {
+                Button {
+                    scheduleNotification()
+                } label: {
+                    Text("Send Notification")
+                }
+
+            }
         }
-        .padding()
+        .onAppear {
+            requestAuthorisation()
+        }
+    }
+    
+    func requestAuthorisation() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound]) { granted, error in
+            self.authorised = granted
+        }
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Hey 👋"
+        content.subtitle = "This is a test notification"
+        content.sound = .default
+        
+        let identifier = UUID().uuidString
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
     }
 }
 
