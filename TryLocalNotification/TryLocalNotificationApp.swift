29a4ef6c7ebbedeccfa34c6504aaafdb0cf7a14c
Weerawut <weerawut@live.com>
2024-12-22 12:23:08 +0700

    Initial Commit

---
+++ TryLocalNotification/TryLocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryLocalNotificationApp.swift
+//  TryLocalNotification
+//
+//  Created by Weerawut Chaiyasomboon on 22/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TryLocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
